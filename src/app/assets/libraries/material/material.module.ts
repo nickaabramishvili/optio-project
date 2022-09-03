a7c8ee4b03b35e9852c9de41157695a0f0f60330
nikabramishvili <n.abramishvili@area.ge>
2022-09-03 22:37:22 +0400

    prog

--- src/app/assets/libraries/material/material.module.ts
+++ src/app/assets/libraries/material/material.module.ts
@@ -2,37 +2,26 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   imports: [
     MatTableModule,
     MatButtonModule,
     MatInputModule,
-    MatListModule,
-    MatRadioModule,
-    MatCheckboxModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
-    MatIconModule,
   ],
   exports: [
     MatTableModule,
     MatButtonModule,
     MatInputModule,
-    MatListModule,
-    MatRadioModule,
-    MatCheckboxModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
-    MatIconModule,
   ],
   providers: [MatDatepickerModule],
 })
