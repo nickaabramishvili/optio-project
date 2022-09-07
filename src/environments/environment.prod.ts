e65d94ddb37981e1fd63450ff9d320ecd68c1595
nikabramishvili <n.abramishvili@area.ge>
2022-09-07 14:59:23 +0400

    done really

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://api.next.insight.optio.ai/api/v2',
+  facts: '/analytics/transactions/facts/aggregate',
+  factsByDay: '/analytics/transactions/facts/find',
 };
